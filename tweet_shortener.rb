6844b1d0c2bfd4ec548dab12131a38f519f67851
William Robert Simpson <will.simpson85@gmail.com>
2019-01-28 18:19:48 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -10,19 +10,16 @@
 #{}"and" becomes "&"
 #{}```
 
-def dictionary(string)
-    string = string.gsub(/\btwo\b/,"2")
-    string = string.gsub(/\btoo\b/,"2")
-    string = string.gsub(/\bto\b/,"2")
-    string = string.gsub(/\bfor\b/,"4")
-    string = string.gsub(/\bfour\b/,"4")
-    string = string.gsub(/\bbe\b/,"b")
-    string = string.gsub(/\byou\b/,"u")
-    string = string.gsub(/\bat\b/,"@")
-    string = string.gsub(/\band\b/,"&")
-    puts "#{string}"
-
+def dictionary()
+  replaceHash = {}
+  replaceHash['hello'] = 'hi'
+  replaceHash[['to','two','too']] = '2'
+  replaceHash[['for','four']] = '4'
+  replaceHash['be']='b'
+  replaceHash['you']='u'
+  replaceHash['at']='@'
+  replaceHash['and']='&'
+  replaceHash
 end
-wordys = "Hey guys, can anyone and teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
-dictionary(wordys)
+puts dictionary
